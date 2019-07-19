43a577520a62952bc40f99e2f35f7712056d7743
Kyle Helle <kjhelle4@gmail.com>
2019-07-18 23:48:33 -0700

    - Add GET /sayHello endpoint - Remove GET / endpoint

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const app = express();
 app.use(cors());    // Note - This enables all CORS requests.
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/sayHello', (req, res) => {
     console.log(`Received request: ${req.method} ${req.url}`);
-    res.send('Hola Mundo!');
+    res.send('Hola Mundo!\n');
+    res.end();
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
